6d839b628c7f10ff552dfd250a64056bb7a669ae
Amit kumar <103359090+nayakamitg@users.noreply.github.com>
2025-03-28 17:04:09 +0530

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import AboutSection from "../components/UI/AboutSection";
+import { Container, Row, Col } from "reactstrap";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+
+import driveImg from "../assets/all-images/drive.jpg";
+import OurMembers from "../components/UI/OurMembers";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <Helmet title="About">
+      <CommonSection title="About Us" />
+      <AboutSection aboutClass="aboutPage" />
+
+      <section className="about__page-section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section__title">
+                  We Are Committed To Provide Safe Ride Solutions
+                </h2>
+
+                <p className="section__description">
+                Looking for a car rental service for your travel needs, business trips, weddings, or long drives? Renting a car offers convenience, flexibility, and affordability, ensuring a smooth and hassle-free experience. Whether you need an economy car, luxury sedan, SUV, or premium sports car, there’s a rental option for every occasion.
+                </p>
+
+                <p className="section__description">
+                Need a reliable, comfortable, and affordable car for your travel needs? Whether it’s a business trip, family vacation, wedding, or weekend getaway, renting a car ensures flexibility, convenience, and a stress-free experience. Choose from a wide range of vehicles, from budget-friendly hatchbacks to luxury sedans and spacious SUVs.
+                </p>
+
+                <div className=" d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section__subtitle">Need Any Help?</h6>
+                    <h4>+916306624037</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <BecomeDriverSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="section__subtitle">Experts</h6>
+              <h2 className="section__title">Our Members</h2>
+            </Col>
+            <OurMembers />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default About;
